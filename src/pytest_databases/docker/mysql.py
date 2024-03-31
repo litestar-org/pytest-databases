1cf25379ae317613737704bbc557efa07b469bf6
Cody Fincher <cody.fincher@gmail.com>
2024-03-31 18:16:10 +0000

    feat: things are configurable

--- src/pytest_databases/docker/mysql.py
+++ src/pytest_databases/docker/mysql.py
@@ -12,33 +12,131 @@ if TYPE_CHECKING:
     from pytest_databases.docker import DockerServiceRegistry
 
 
-async def mysql_responsive(host: str, port: int) -> bool:
+async def mysql_responsive(host: str, port: int, user: str, password: str, database: str) -> bool:
     try:
         conn = await asyncmy.connect(
             host=host,
             port=port,
-            user="app",
-            database="db",
-            password="super-secret",
+            user=user,
+            database=database,
+            password=password,
         )
+    except Exception:  # noqa: BLE001
+        return False
+
+    try:
         async with conn.cursor() as cursor:
             await cursor.execute("select 1 as is_available")
             resp = await cursor.fetchone()
         return resp[0] == 1
-    except asyncmy.errors.OperationalError:
-        return False
+    finally:
+        await conn.close()
 
 
 @pytest.fixture()
-async def mysql8_service(docker_services: DockerServiceRegistry) -> None:
-    await docker_services.start("mysql8", timeout=45, pause=1, check=mysql_responsive, port=3360)
+def mysql_user() -> str:
+    return "app"
 
 
 @pytest.fixture()
-async def mysql57_service(docker_services: DockerServiceRegistry) -> None:
-    await docker_services.start("mysql57", timeout=45, pause=1, check=mysql_responsive, port=3363)
+def mysql_password() -> str:
+    return "super-secret"
 
 
 @pytest.fixture()
-async def mysql56_service(docker_services: DockerServiceRegistry) -> None:
-    await docker_services.start("mysql56", timeout=45, pause=1, check=mysql_responsive, port=3362)
+def mysql_database() -> str:
+    return "db"
+
+
+@pytest.fixture()
+def mysql56_port() -> int:
+    return 3362
+
+
+@pytest.fixture()
+def mysql57_port() -> int:
+    return 3361
+
+
+@pytest.fixture()
+def mysql8_port() -> int:
+    return 3360
+
+
+@pytest.fixture()
+def mysql_default_version() -> str:
+    return "mysql8"
+
+
+@pytest.fixture()
+def mysql_port(mysql8_port: int) -> int:
+    return mysql8_port
+
+
+@pytest.fixture()
+async def mysql8_service(
+    docker_services: DockerServiceRegistry, mysql8_port: int, mysql_database: str, mysql_user: str, mysql_password: str
+) -> None:
+    await docker_services.start(
+        "mysql8",
+        timeout=45,
+        pause=1,
+        check=mysql_responsive,
+        port=mysql8_port,
+        database=mysql_database,
+        user=mysql_user,
+        password=mysql_password,
+    )
+
+
+@pytest.fixture()
+async def mysql57_service(
+    docker_services: DockerServiceRegistry, mysql57_port: int, mysql_database: str, mysql_user: str, mysql_password: str
+) -> None:
+    await docker_services.start(
+        "mysql57",
+        timeout=45,
+        pause=1,
+        check=mysql_responsive,
+        port=mysql57_port,
+        database=mysql_database,
+        user=mysql_user,
+        password=mysql_password,
+    )
+
+
+@pytest.fixture()
+async def mysql56_service(
+    docker_services: DockerServiceRegistry, mysql56_port: int, mysql_database: str, mysql_user: str, mysql_password: str
+) -> None:
+    await docker_services.start(
+        "mysql56",
+        timeout=45,
+        pause=1,
+        check=mysql_responsive,
+        port=mysql56_port,
+        database=mysql_database,
+        user=mysql_user,
+        password=mysql_password,
+    )
+
+
+@pytest.fixture()
+async def mysql_service(
+    docker_services: DockerServiceRegistry,
+    mysql_default_version: str,
+    mysql_port: int,
+    mysql_database: str,
+    mysql_user: str,
+    mysql_password: str,
+) -> None:
+    await docker_services.start(
+        mysql_default_version,
+        timeout=45,
+        pause=1,
+        check=mysql_responsive,
+        port=mysql_port,
+        database=mysql_database,
+        user=mysql_user,
+        password=mysql_password,
+    )
--- src/pytest_databases/docker/postgres.py
+++ src/pytest_databases/docker/postgres.py
@@ -12,14 +12,14 @@ if TYPE_CHECKING:
     from pytest_databases.docker import DockerServiceRegistry
 
 
-async def postgres_responsive(host: str, port: int) -> bool:
+async def postgres_responsive(host: str, port: int, user: str, password: str, database: str) -> bool:
     try:
         conn = await asyncpg.connect(
             host=host,
             port=port,
-            user="postgres",
-            database="postgres",
-            password="super-secret",
+            user=user,
+            database=database,
+            password=password,
         )
     except Exception:  # noqa: BLE001
         return False
@@ -32,25 +32,177 @@ async def postgres_responsive(host: str, port: int) -> bool:
 
 
 @pytest.fixture()
-async def postgres12_service(docker_services: DockerServiceRegistry) -> None:
-    await docker_services.start("postgres12", timeout=45, pause=1, check=postgres_responsive, port=5423)
+def postgres_user() -> str:
+    return "postgres"
 
 
 @pytest.fixture()
-async def postgres13_service(docker_services: DockerServiceRegistry) -> None:
-    await docker_services.start("postgres13", timeout=45, pause=1, check=postgres_responsive, port=5424)
+def postgres_password() -> str:
+    return "super-secret"
 
 
 @pytest.fixture()
-async def postgres14_service(docker_services: DockerServiceRegistry) -> None:
-    await docker_services.start("postgres14", timeout=45, pause=1, check=postgres_responsive, port=5425)
+def postgres_database() -> str:
+    return "postgres"
 
 
 @pytest.fixture()
-async def postgres15_service(docker_services: DockerServiceRegistry) -> None:
-    await docker_services.start("postgres15", timeout=45, pause=1, check=postgres_responsive, port=5426)
+def postgres11_port() -> int:
+    return 5422
 
 
 @pytest.fixture()
-async def postgres16_service(docker_services: DockerServiceRegistry) -> None:
-    await docker_services.start("postgres16", timeout=45, pause=1, check=postgres_responsive, port=5427)
+def postgres12_port() -> int:
+    return 5423
+
+
+@pytest.fixture()
+def postgres13_port() -> int:
+    return 5424
+
+
+@pytest.fixture()
+def postgres14_port() -> int:
+    return 5425
+
+
+@pytest.fixture()
+def postgres15_port() -> int:
+    return 5426
+
+
+@pytest.fixture()
+def postgres16_port() -> int:
+    return 5427
+
+
+@pytest.fixture()
+def postgres_default_version() -> str:
+    return "postgres16"
+
+
+@pytest.fixture()
+def postgres_port(postgres16_port: int) -> int:
+    return postgres16_port
+
+
+@pytest.fixture()
+async def postgres12_service(
+    docker_services: DockerServiceRegistry,
+    postgres12_port: int,
+    postgres_database: str,
+    postgres_user: str,
+    postgres_password: str,
+) -> None:
+    await docker_services.start(
+        "postgres12",
+        timeout=45,
+        pause=1,
+        check=postgres_responsive,
+        port=postgres12_port,
+        database=postgres_database,
+        user=postgres_user,
+        password=postgres_password,
+    )
+
+
+@pytest.fixture()
+async def postgres13_service(
+    docker_services: DockerServiceRegistry,
+    postgres13_port: int,
+    postgres_database: str,
+    postgres_user: str,
+    postgres_password: str,
+) -> None:
+    await docker_services.start(
+        "postgres13",
+        timeout=45,
+        pause=1,
+        check=postgres_responsive,
+        port=postgres13_port,
+        database=postgres_database,
+        user=postgres_user,
+        password=postgres_password,
+    )
+
+
+@pytest.fixture()
+async def postgres14_service(
+    docker_services: DockerServiceRegistry,
+    postgres14_port: int,
+    postgres_database: str,
+    postgres_user: str,
+    postgres_password: str,
+) -> None:
+    await docker_services.start(
+        "postgres14",
+        timeout=45,
+        pause=1,
+        check=postgres_responsive,
+        port=postgres14_port,
+        database=postgres_database,
+        user=postgres_user,
+        password=postgres_password,
+    )
+
+
+@pytest.fixture()
+async def postgres15_service(
+    docker_services: DockerServiceRegistry,
+    postgres15_port: int,
+    postgres_database: str,
+    postgres_user: str,
+    postgres_password: str,
+) -> None:
+    await docker_services.start(
+        "postgres15",
+        timeout=45,
+        pause=1,
+        check=postgres_responsive,
+        port=postgres15_port,
+        database=postgres_database,
+        user=postgres_user,
+        password=postgres_password,
+    )
+
+
+@pytest.fixture()
+async def postgres16_service(
+    docker_services: DockerServiceRegistry,
+    postgres16_port: int,
+    postgres_database: str,
+    postgres_user: str,
+    postgres_password: str,
+) -> None:
+    await docker_services.start(
+        "postgres16",
+        timeout=45,
+        pause=1,
+        check=postgres_responsive,
+        port=postgres16_port,
+        database=postgres_database,
+        user=postgres_user,
+        password=postgres_password,
+    )
+
+
+# alias to the latest
+@pytest.fixture()
+async def postgres_service(
+    docker_services: DockerServiceRegistry,
+    postgres_default_version: str,
+    postgres_port: int,
+    postgres_database: str,
+    postgres_user: str,
+    postgres_password: str,
+) -> None:
+    await docker_services.start(
+        postgres_default_version,
+        timeout=45,
+        pause=1,
+        check=postgres_responsive,
+        port=postgres_port,
+        database=postgres_database,
+        user=postgres_user,
+        password=postgres_password,
+    )
