35876df62eaa23004d5a619e1b3ebba8b3755a27
Cody Fincher <cody.fincher@gmail.com>
2024-03-31 21:30:49 +0000

    fix: additional build updates

--- src/pytest_databases/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present Jolt
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.1.0"
---
+++ src/pytest_databases/__metadata__.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2023-present Jolt
+#
+# SPDX-License-Identifier: MIT
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, metadata, version
+
+__all__ = ("__project__", "__version__")
+
+try:
+    __version__ = version("pytest-databases")
+    """Version of the project."""
+    __project__ = metadata("pytest-databases")["Name"]
+    """Name of the project."""
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
+    __project__ = "pytest-databases"
+finally:
+    del version, PackageNotFoundError, metadata
