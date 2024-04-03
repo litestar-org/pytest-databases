b4fe877cf6ca7fe68698682fb92a9e1e5b5ea017
Cody Fincher <cody.fincher@gmail.com>
2024-04-03 22:18:55 +0000

    chore(ci): enable license header requirement

--- scripts/build_docs.py
+++ scripts/build_docs.py
@@ -1,3 +1,26 @@
+# MIT License
+
+# Copyright (c) 2024 Litestar
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 from __future__ import annotations
 
 import argparse
@@ -36,13 +59,13 @@ def checkout(branch: str) -> Generator[None, None, None]:
 def build(output_dir: str) -> None:
     subprocess.run(["make", "docs"], check=True)  # noqa: S607
 
-    output_dir = Path(output_dir)
-    output_dir.mkdir()
-    output_dir.joinpath(".nojekyll").touch(exist_ok=True)
-    output_dir.joinpath("index.html").write_text(REDIRECT_TEMPLATE.format(target="latest"))
+    output_path = Path(output_dir)
+    output_path.mkdir()
+    output_path.joinpath(".nojekyll").touch(exist_ok=True)
+    output_path.joinpath("index.html").write_text(REDIRECT_TEMPLATE.format(target="latest"))
 
     docs_src_path = Path("docs/_build/html")
-    shutil.copytree(docs_src_path, output_dir / "latest", dirs_exist_ok=True)
+    shutil.copytree(docs_src_path, output_path / "latest", dirs_exist_ok=True)
 
 
 def main() -> None:
