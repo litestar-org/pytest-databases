b6853eaeee6369b46e6f9350d0425a5961e345c0
Janek Nouvertné <provinzkraut@posteo.de>
2024-10-20 02:26:57 +0200

    feat: Remove volumes when stopping services (#52)

--- src/pytest_databases/docker/dragonfly.py
+++ src/pytest_databases/docker/dragonfly.py
@@ -29,11 +29,8 @@ def dragonfly_docker_services(
     if os.getenv("GITHUB_ACTIONS") == "true" and sys.platform != "linux":
         pytest.skip("Docker not available on this platform")
 
-    registry = DockerServiceRegistry(worker_id, compose_project_name=dragonfly_compose_project_name)
-    try:
+    with DockerServiceRegistry(worker_id, compose_project_name=dragonfly_compose_project_name) as registry:
         yield registry
-    finally:
-        registry.down()
 
 
 @pytest.fixture(scope="session")
