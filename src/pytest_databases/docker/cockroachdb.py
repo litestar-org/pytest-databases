0d24b5719f6891e02d0c4d1cde43cdd1b33b762f
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-11-10 13:20:09 +0100

    migrate cockroach

--- src/pytest_databases/docker/cockroachdb.py
+++ src/pytest_databases/docker/cockroachdb.py
@@ -2,60 +2,26 @@ from __future__ import annotations
 
 import os
 import sys
+from dataclasses import dataclass
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 import psycopg
 import pytest
 
+from pytest_databases._service import DockerService, ServiceContainer
 from pytest_databases.docker import DockerServiceRegistry
-from pytest_databases.helpers import simple_string_hash
+from pytest_databases.docker.dragonfly import dragonfly_image
+from pytest_databases.helpers import simple_string_hash, get_xdist_worker_num
 
 if TYPE_CHECKING:
     from collections.abc import Generator
 
 
-COMPOSE_PROJECT_NAME: str = f"pytest-databases-cockroachdb-{simple_string_hash(__file__)}"
-
-
-def cockroachdb_responsive(host: str, port: int, database: str, driver_opts: dict[str, str]) -> bool:
-    opts = "&".join(f"{k}={v}" for k, v in driver_opts.items()) if driver_opts else ""
-    try:
-        conn = psycopg.connect(f"postgresql://root@{host}:{port}/{database}?{opts}")
-    except Exception:  # noqa: BLE001
-        return False
-
-    try:
-        db_open = conn.execute("SELECT 1").fetchone()
-        return bool(db_open is not None and db_open[0] == 1)
-    finally:
-        conn.close()
-
-
-@pytest.fixture(scope="session")
-def cockroachdb_compose_project_name() -> str:
-    return os.environ.get("COMPOSE_PROJECT_NAME", COMPOSE_PROJECT_NAME)
-
-
-@pytest.fixture(autouse=False, scope="session")
-def cockroachdb_docker_services(
-    cockroachdb_compose_project_name: str, worker_id: str = "main"
-) -> Generator[DockerServiceRegistry, None, None]:
-    if os.getenv("GITHUB_ACTIONS") == "true" and sys.platform != "linux":
-        pytest.skip("Docker not available on this platform")
-
-    with DockerServiceRegistry(worker_id, compose_project_name=cockroachdb_compose_project_name) as registry:
-        yield registry
-
-
-@pytest.fixture(scope="session")
-def cockroachdb_port() -> int:
-    return 26257
-
-
-@pytest.fixture(scope="session")
-def cockroachdb_database() -> str:
-    return "defaultdb"
+@dataclass
+class CockroachDBService(ServiceContainer):
+    database: str
+    driver_opts: dict[str, str]
 
 
 @pytest.fixture(scope="session")
@@ -63,55 +29,50 @@ def cockroachdb_driver_opts() -> dict[str, str]:
     return {"sslmode": "disable"}
 
 
-@pytest.fixture(scope="session")
-def cockroachdb_docker_compose_files() -> list[Path]:
-    return [Path(Path(__file__).parent / "docker-compose.cockroachdb.yml")]
-
-
-@pytest.fixture(scope="session")
-def default_cockroachdb_service_name() -> str:
-    return "cockroachdb"
-
-
-@pytest.fixture(scope="session")
-def cockroachdb_docker_ip(cockroachdb_docker_services: DockerServiceRegistry) -> str:
-    return cockroachdb_docker_services.docker_ip
-
-
 @pytest.fixture(autouse=False, scope="session")
 def cockroachdb_service(
-    cockroachdb_docker_services: DockerServiceRegistry,
-    default_cockroachdb_service_name: str,
-    cockroachdb_docker_compose_files: list[Path],
-    cockroachdb_port: int,
-    cockroachdb_database: str,
+    docker_service: DockerService,
     cockroachdb_driver_opts: dict[str, str],
-) -> Generator[None, None, None]:
-    os.environ["COCKROACHDB_DATABASE"] = cockroachdb_database
-    os.environ["COCKROACHDB_PORT"] = str(cockroachdb_port)
-    cockroachdb_docker_services.start(
-        name=default_cockroachdb_service_name,
-        docker_compose_files=cockroachdb_docker_compose_files,
-        timeout=60,
-        pause=1,
+) -> Generator[CockroachDBService, None, None]:
+    def cockroachdb_responsive(_service: ServiceContainer) -> bool:
+        opts = "&".join(f"{k}={v}" for k, v in cockroachdb_driver_opts.items()) if cockroachdb_driver_opts else ""
+        try:
+            conn = psycopg.connect(f"postgresql://root@{_service.host}:{_service.port}/defaultdb?{opts}")
+        except Exception:  # noqa: BLE001
+            return False
+
+        try:
+            db_open = conn.execute("SELECT 1").fetchone()
+            return bool(db_open is not None and db_open[0] == 1)
+        finally:
+            conn.close()
+
+    worker_num = get_xdist_worker_num()
+    db_name = f"pytest_{worker_num + 1}"
+
+    with docker_service.run(
+        image="cockroachdb/cockroach:latest",
+        container_port=26257,
         check=cockroachdb_responsive,
-        port=cockroachdb_port,
-        database=cockroachdb_database,
-        driver_opts=cockroachdb_driver_opts,
-    )
-    yield
+        name="cockroachdb",
+        command="start-single-node --insecure",
+        exec_after_start=f'cockroach sql --insecure -e "CREATE DATABASE {db_name}";',
+    ) as service:
+        yield CockroachDBService(
+            host=service.host,
+            port=service.port,
+            database=db_name,
+            driver_opts=cockroachdb_driver_opts,
+        )
 
 
 @pytest.fixture(autouse=False, scope="session")
 def cockroachdb_startup_connection(
-    cockroachdb_service: DockerServiceRegistry,
-    cockroachdb_docker_ip: str,
-    cockroachdb_port: int,
-    cockroachdb_database: str,
+    cockroachdb_service: CockroachDBService,
     cockroachdb_driver_opts: dict[str, str],
 ) -> Generator[psycopg.Connection, None, None]:
     opts = "&".join(f"{k}={v}" for k, v in cockroachdb_driver_opts.items()) if cockroachdb_driver_opts else ""
     with psycopg.connect(
-        f"postgresql://root@{cockroachdb_docker_ip}:{cockroachdb_port}/{cockroachdb_database}?{opts}"
+        f"postgresql://root@{cockroachdb_service.host}:{cockroachdb_service.port}/{cockroachdb_service.database}?{opts}"
     ) as conn:
         yield conn
--- tests/docker/test_cockroachdb.py
+++ tests/docker/test_cockroachdb.py
@@ -2,37 +2,32 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from pytest_databases.docker.cockroachdb import cockroachdb_responsive
+import psycopg
+
+from pytest_databases.docker.cockroachdb import CockroachDBService
 
 if TYPE_CHECKING:
-    import psycopg
-
-    from pytest_databases.docker import DockerServiceRegistry
+    pass
 
 pytest_plugins = [
     "pytest_databases.docker.cockroachdb",
 ]
 
 
-def test_cockroachdb_default_config(
-    cockroachdb_port: int, cockroachdb_database: str, cockroachdb_driver_opts: dict[str, str]
-) -> None:
-    assert cockroachdb_port == 26257
-    assert cockroachdb_database == "defaultdb"
+def test_cockroachdb_default_config(cockroachdb_driver_opts: dict[str, str]) -> None:
     assert cockroachdb_driver_opts == {"sslmode": "disable"}
 
 
 def test_cockroachdb_service(
-    cockroachdb_docker_ip: str,
-    cockroachdb_service: DockerServiceRegistry,
-    cockroachdb_database: str,
-    cockroachdb_port: int,
-    cockroachdb_driver_opts: dict[str, str],
+    cockroachdb_service: CockroachDBService,
 ) -> None:
-    ping = cockroachdb_responsive(
-        cockroachdb_docker_ip, cockroachdb_port, cockroachdb_database, cockroachdb_driver_opts
-    )
-    assert ping
+    opts = "&".join(f"{k}={v}" for k, v in cockroachdb_service.driver_opts.items())
+    with psycopg.connect(
+        f"postgresql://root@{cockroachdb_service.host}:{cockroachdb_service.port}/{cockroachdb_service.database}?{opts}"
+    ) as conn:
+        conn.execute("CREATE TABLE if not exists simple_table as SELECT 1 as the_value")
+        result = conn.execute("select * from simple_table").fetchone()
+        assert result is not None and result[0] == 1
 
 
 def test_cockroachdb_services_after_start(
@@ -40,4 +35,4 @@ def test_cockroachdb_services_after_start(
 ) -> None:
     cockroachdb_startup_connection.execute("CREATE TABLE if not exists simple_table as SELECT 1 as the_value")
     result = cockroachdb_startup_connection.execute("select * from simple_table").fetchone()
-    assert bool(result is not None and result[0] == 1)
+    assert result is not None and result[0] == 1
