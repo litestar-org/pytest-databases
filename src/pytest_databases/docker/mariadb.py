7a00456291d079f4c959b42d7fb50761282fffc0
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-11-10 12:00:14 +0100

    migrate mariadb

--- src/pytest_databases/docker/mariadb.py
+++ src/pytest_databases/docker/mariadb.py
@@ -1,177 +1,120 @@
 from __future__ import annotations
 
-import os
-import sys
-from pathlib import Path
-from typing import TYPE_CHECKING, Any, Generator
+import contextlib
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Generator
 
 import pymysql
 import pytest
 
-from pytest_databases.docker import DockerServiceRegistry
-from pytest_databases.docker.mysql import mysql_responsive
-from pytest_databases.helpers import simple_string_hash
+from pytest_databases._service import DockerService
+from pytest_databases.helpers import get_xdist_worker_num
+from pytest_databases.types import ServiceContainer
 
 if TYPE_CHECKING:
     from collections.abc import Generator
 
 
-COMPOSE_PROJECT_NAME: str = f"pytest-databases-mariadb-{simple_string_hash(__file__)}"
+@dataclass
+class MariaDBService(ServiceContainer):
+    db: str
+    user: str
+    password: str
 
 
-@pytest.fixture(scope="session")
-def mariadb_compose_project_name() -> str:
-    return os.environ.get("COMPOSE_PROJECT_NAME", COMPOSE_PROJECT_NAME)
+@contextlib.contextmanager
+def _provide_mysql_service(
+    docker_service: DockerService,
+    image: str,
+    name: str,
+) -> Generator[MariaDBService, None, None]:
+    user = "app"
+    password = "super-secret"
+    root_password = "super-secret"
+    database = "db"
 
+    def check(_service: ServiceContainer) -> bool:
+        try:
+            conn = pymysql.connect(
+                host=_service.host,
+                port=_service.port,
+                user=user,
+                database=database,
+                password=password,
+            )
+        except Exception:  # noqa: BLE001
+            return False
 
-@pytest.fixture(autouse=False, scope="session")
-def mariadb_docker_services(
-    mariadb_compose_project_name: str, worker_id: str = "main"
-) -> Generator[DockerServiceRegistry, None, None]:
-    if os.getenv("GITHUB_ACTIONS") == "true" and sys.platform != "linux":
-        pytest.skip("Docker not available on this platform")
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute("select 1 as is_available")
+                resp = cursor.fetchone()
+                return resp is not None and resp[0] == 1
+        finally:
+            with contextlib.suppress(Exception):
+                conn.close()
 
-    with DockerServiceRegistry(worker_id, compose_project_name=mariadb_compose_project_name) as registry:
-        yield registry
-
-
-@pytest.fixture(scope="session")
-def mariadb_user() -> str:
-    return "app"
-
-
-@pytest.fixture(scope="session")
-def mariadb_password() -> str:
-    return "super-secret"
-
-
-@pytest.fixture(scope="session")
-def mariadb_root_password() -> str:
-    return "super-secret"
-
-
-@pytest.fixture(scope="session")
-def mariadb_database() -> str:
-    return "db"
-
-
-@pytest.fixture(scope="session")
-def mariadb113_port() -> int:
-    return 3359
-
-
-@pytest.fixture(scope="session")
-def default_mariadb_service_name() -> str:
-    return "mariadb113"
-
-
-@pytest.fixture(scope="session")
-def mariadb_port(mariadb113_port: int) -> int:
-    return mariadb113_port
-
-
-@pytest.fixture(scope="session")
-def mariadb_docker_compose_files() -> list[Path]:
-    return [Path(Path(__file__).parent / "docker-compose.mariadb.yml")]
-
-
-@pytest.fixture(scope="session")
-def mariadb_docker_ip(mariadb_docker_services: DockerServiceRegistry) -> str:
-    return mariadb_docker_services.docker_ip
+    worker_num = get_xdist_worker_num()
+    db_name = f"pytest_{worker_num + 1}"
+    with docker_service.run(
+        image=image,
+        check=check,
+        container_port=3306,
+        name=name,
+        env={
+            "MARIADB_ROOT_PASSWORD": root_password,
+            "MARIADB_PASSWORD": password,
+            "MARIADB_USER": user,
+            "MARIADB_DATABASE": database,
+            "MARIADB_ROOT_HOST": "%",
+            "LANG": "C.UTF-8",
+        },
+        timeout=60,
+        pause=0.5,
+        exec_after_start=(
+            f'mariadb --user=root --password={root_password} -e "CREATE DATABASE {db_name};'
+            f"GRANT ALL PRIVILEGES ON {db_name}.* TO '{user}'@'%'; "
+            'FLUSH PRIVILEGES;"'
+        ),
+    ) as service:
+        yield MariaDBService(
+            db=db_name,
+            host=service.host,
+            port=service.port,
+            user=user,
+            password=password,
+        )
 
 
 @pytest.fixture(autouse=False, scope="session")
 def mariadb113_service(
-    mariadb_docker_services: DockerServiceRegistry,
-    mariadb_docker_compose_files: list[Path],
-    mariadb113_port: int,
-    mariadb_database: str,
-    mariadb_user: str,
-    mariadb_password: str,
-    mariadb_root_password: str,
-) -> Generator[None, None, None]:
-    os.environ["MARIADB_ROOT_PASSWORD"] = mariadb_root_password
-    os.environ["MARIADB_PASSWORD"] = mariadb_password
-    os.environ["MARIADB_USER"] = mariadb_user
-    os.environ["MARIADB_DATABASE"] = mariadb_database
-    os.environ["MARIADB113_PORT"] = str(mariadb113_port)
-    mariadb_docker_services.start(
-        "mariadb113",
-        docker_compose_files=mariadb_docker_compose_files,
-        timeout=45,
-        pause=1,
-        check=mysql_responsive,
-        port=mariadb113_port,
-        database=mariadb_database,
-        user=mariadb_user,
-        password=mariadb_password,
-    )
-    yield
+    docker_service: DockerService,
+) -> Generator[MariaDBService, None, None]:
+    with _provide_mysql_service(
+        docker_service=docker_service,
+        image="mariadb:11.3",
+        name="mariadb-11.3",
+    ) as service:
+        yield service
 
 
 @pytest.fixture(autouse=False, scope="session")
-def mariadb_service(
-    mariadb_docker_services: DockerServiceRegistry,
-    default_mariadb_service_name: str,
-    mariadb_docker_compose_files: list[Path],
-    mariadb_port: int,
-    mariadb_database: str,
-    mariadb_user: str,
-    mariadb_password: str,
-    mariadb_root_password: str,
-) -> Generator[None, None, None]:
-    os.environ["MARIADB_ROOT_PASSWORD"] = mariadb_root_password
-    os.environ["MARIADB_PASSWORD"] = mariadb_password
-    os.environ["MARIADB_USER"] = mariadb_user
-    os.environ["MARIADB_DATABASE"] = mariadb_database
-    os.environ[f"{default_mariadb_service_name.upper()}_PORT"] = str(mariadb_port)
-    mariadb_docker_services.start(
-        name=default_mariadb_service_name,
-        docker_compose_files=mariadb_docker_compose_files,
-        timeout=45,
-        pause=1,
-        check=mysql_responsive,
-        port=mariadb_port,
-        database=mariadb_database,
-        user=mariadb_user,
-        password=mariadb_password,
-    )
-    yield
+def mariadb_service(mariadb113_service: MariaDBService) -> MariaDBService:
+    return mariadb113_service
 
 
 @pytest.fixture(autouse=False, scope="session")
-def mariadb_startup_connection(
-    mariadb_service: DockerServiceRegistry,
-    mariadb_docker_ip: str,
-    mariadb_port: int,
-    mariadb_database: str,
-    mariadb_user: str,
-    mariadb_password: str,
-) -> Generator[Any, None, None]:
+def mariadb113_startup_connection(mariadb_service: MariaDBService) -> Generator[pymysql.Connection, None, None]:
     with pymysql.connect(
-        host=mariadb_docker_ip,
-        port=mariadb_port,
-        user=mariadb_user,
-        database=mariadb_database,
-        password=mariadb_password,
+        host=mariadb_service.host,
+        port=mariadb_service.port,
+        user=mariadb_service.user,
+        database=mariadb_service.db,
+        password=mariadb_service.password,
     ) as conn:
         yield conn
 
 
 @pytest.fixture(autouse=False, scope="session")
-def mariadb113_startup_connection(
-    mariadb113_service: DockerServiceRegistry,
-    mariadb_docker_ip: str,
-    mariadb113_port: int,
-    mariadb_database: str,
-    mariadb_user: str,
-    mariadb_password: str,
-) -> Generator[Any, None, None]:
-    with pymysql.connect(
-        host=mariadb_docker_ip,
-        port=mariadb113_port,
-        user=mariadb_user,
-        database=mariadb_database,
-        password=mariadb_password,
-    ) as conn:
-        yield conn
+def mariadb_startup_connection(mariadb113_startup_connection: pymysql.Connection) -> pymysql.Connection:
+    return mariadb113_startup_connection
