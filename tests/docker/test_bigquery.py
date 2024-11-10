f8f7194627008786746c35988f4cc62c1882f921
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-11-10 14:16:32 +0100

    migrate bigquery

--- tests/docker/test_bigquery.py
+++ tests/docker/test_bigquery.py
@@ -4,52 +4,27 @@ from typing import TYPE_CHECKING
 
 from google.cloud import bigquery
 
-from pytest_databases.docker.bigquery import bigquery_responsive
+from pytest_databases.docker.bigquery import BigQueryService
 
 if TYPE_CHECKING:
-    from google.api_core.client_options import ClientOptions
-    from google.auth.credentials import Credentials
-
-    from pytest_databases.docker import DockerServiceRegistry
+    pass
 
 pytest_plugins = [
     "pytest_databases.docker.bigquery",
 ]
 
 
-def test_bigquery_default_config(
-    bigquery_docker_ip: str,
-    bigquery_port: int,
-    bigquery_grpc_port: int,
-    bigquery_dataset: str,
-    bigquery_endpoint: str,
-    bigquery_project: str,
-) -> None:
-    assert bigquery_port == 9051
-    assert bigquery_grpc_port == 9061
-    assert bigquery_dataset == "test-dataset"
-    assert bigquery_endpoint == f"http://{bigquery_docker_ip}:9051"
-    assert bigquery_project == "emulator-test-project"
-
-
-def test_bigquery_services(
-    bigquery_docker_ip: str,
-    bigquery_service: DockerServiceRegistry,
-    bigquery_endpoint: str,
-    bigquery_dataset: str,
-    bigquery_client_options: ClientOptions,
-    bigquery_project: str,
-    bigquery_credentials: Credentials,
-) -> None:
-    ping = bigquery_responsive(
-        bigquery_docker_ip,
-        bigquery_endpoint=bigquery_endpoint,
-        bigquery_dataset=bigquery_dataset,
-        bigquery_project=bigquery_project,
-        bigquery_credentials=bigquery_credentials,
-        bigquery_client_options=bigquery_client_options,
+def test_bigquery_service(bigquery_service: BigQueryService) -> None:
+    client = bigquery.Client(
+        project=bigquery_service.project,
+        client_options=bigquery_service.client_options,
+        credentials=bigquery_service.credentials,
     )
-    assert ping
+
+    job = client.query(query="SELECT 1 as one")
+
+    resp = list(job.result())
+    assert resp[0].one == 1
 
 
 def test_bigquery_service_after_start(
