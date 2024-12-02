7505564cad6aecedb8eb80a8f28c48531c1dc5dd
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-12-02 20:40:22 +0100

    isolate mariadb

--- src/pytest_databases/docker/mariadb.py
+++ src/pytest_databases/docker/mariadb.py
@@ -61,7 +61,7 @@ def _provide_mysql_service(
         image=image,
         check=check,
         container_port=3306,
-        name=name,
+        name=f"{name}_{worker_num}",
         env={
             "MARIADB_ROOT_PASSWORD": root_password,
             "MARIADB_PASSWORD": password,
