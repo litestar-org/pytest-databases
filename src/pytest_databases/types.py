d21f63ed53b15a1912a664a9e8fd842f2971b64b
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-12-02 20:35:05 +0100

    bigquery isolation

--- src/pytest_databases/types.py
+++ src/pytest_databases/types.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
 import dataclasses
+from typing import Literal
 
 
 @dataclasses.dataclass
 class ServiceContainer:
     host: str
     port: int
+
+
+XdistIsolationLevel = Literal["database", "server"]
